cfe06d3def2a8f669c85609cf1508145c631ef0e
Clayton Davis <cd@ae4ax.net>
2017-04-19 22:57:17 -0400

    Move projects

--- src/Claytondus.EasyPost/Properties/AssemblyInfo.cs
+++ src/Claytondus.EasyPost/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Copyright 2017 Clayton Davis and EasyPost")]
 [assembly: AssemblyProduct("Claytondus.EasyPost")]
 [assembly: AssemblyTrademark("")]
 
